f89ae1e8a2057295399cf2f370f11ac9d4ce48d8
Austin Goetz <a.goetz09@gmail.com>
2019-09-20 14:58:22 -0600

    first commit

---
+++ GitLesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitLesson
+//
+//  Created by Austin Goetz on 9/20/19.
+//  Copyright © 2019 Austin Goetz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
